7671672488abe634bf66c73f8e3000129b0aeeba
Benjamin Joham <bjoham.mmt-b2017@fh-salzburg.ac.at>
2019-01-22 00:31:16 +0100

    different view for different categories

--- public/wp-content/themes/alex-mayer/category.php
+++ public/wp-content/themes/alex-mayer/category.php
@@ -3,7 +3,7 @@
   <section id="<?= $category_name; ?>">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <article>
-      <h2><a href='<?php echo get_permalink(); ?>'><?php the_title(); ?></a></h2>
+      <h3>><?php the_title(); ?></h3>
       <?php 
         if ($category_name == 'leistungen') {
           the_post_thumbnail();
--- public/wp-content/themes/alex-mayer/functions.php
+++ public/wp-content/themes/alex-mayer/functions.php
@@ -1,8 +1,8 @@
 <?php
 // enqueue scripts and styles
 function enqueue_scripts_and_styles() {
-    wp_enqueue_style('main-styles', get_template_directory_uri().'/style.c51e59ab02b75cc07f14.css?ac13032ef64f3fd5fdbd', array(), null, false);
-    wp_enqueue_script( 'main-js', get_template_directory_uri().'/main.1a058b97656bcdc19f0b.js?ac13032ef64f3fd5fdbd', array(), null, false );
+    wp_enqueue_style('main-styles', get_template_directory_uri().'/style.18b188decdd06e09bd5e.css?deddb9b098020468e923', array(), null, false);
+    wp_enqueue_script( 'main-js', get_template_directory_uri().'/main.1f3b7c6dc1c68fbbed0d.js?deddb9b098020468e923', array(), null, false );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_scripts_and_styles' );
 
