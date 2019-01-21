15e99776a0282c826dd4930eeaeb979367308484
Benjamin Joham <bjoham.mmt-b2017@fh-salzburg.ac.at>
2019-01-22 00:33:12 +0100

    different view for different categories

--- public/wp-content/themes/alex-mayer/functions.php
+++ public/wp-content/themes/alex-mayer/functions.php
@@ -1,8 +1,8 @@
 <?php
 // enqueue scripts and styles
 function enqueue_scripts_and_styles() {
-    wp_enqueue_style('main-styles', get_template_directory_uri().'/style.c51e59ab02b75cc07f14.css?ee53965c69fb2827ebbd', array(), null, false);
-    wp_enqueue_script( 'main-js', get_template_directory_uri().'/main.1a058b97656bcdc19f0b.js?ee53965c69fb2827ebbd', array(), null, false );
+    wp_enqueue_style('main-styles', get_template_directory_uri().'/style.c51e59ab02b75cc07f14.css?aea55974689ae2126591', array(), null, false);
+    wp_enqueue_script( 'main-js', get_template_directory_uri().'/main.1a058b97656bcdc19f0b.js?aea55974689ae2126591', array(), null, false );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_scripts_and_styles' );
 
